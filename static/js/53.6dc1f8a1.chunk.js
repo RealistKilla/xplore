3aa4c3adb9f4b376b32fb085a0df738d6c2c7af7
RealistKilla <tyler.thms@gmail.com>
2020-12-01 00:01:42 +0200

    Add files via upload
    Initial Commit

---
+++ static/js/53.6dc1f8a1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpxplore=this.webpackJsonpxplore||[]).push([[53],{135:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(13),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var r=i.closest("ion-content");r&&r.componentOnReady().then((function(){Object(o.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.6dc1f8a1.chunk.js.map
